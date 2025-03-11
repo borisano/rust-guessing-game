25f3d005fa2315b0426ac8309c5179864b5cda3e
Anatolii Borysov <borisano@gmail.com>
2025-03-11 09:47:16 +0000

    Put guessing code in a loop

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,25 @@ fn main() {
 
     println!("Secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    let guess : u32 = guess.trim().parse().expect("Please type a number");
+        let guess : u32 = guess.trim().parse().expect("Please type a number");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You guessed right!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed right!");
+                break;
+            },
+        }
     }
 
 }
